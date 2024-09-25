d8bbf767de0d4cac30f60e7fa333d46a9794def4
xinlei.zhao <xinlei.zhao1@cocos.com>
2024-09-26 00:42:07 +0800

    fix

--- getrule.sh
+++ getrule.sh
@@ -37,8 +37,8 @@ geodat_update
 v2dat_dump
 update_local_ptr
 
-touch $v2dat_dir/rules/force-nocn.txt
-touch $v2dat_dir/rules/force-cn.txt
+#touch $v2dat_dir/rules/force-nocn.txt
+#touch $v2dat_dir/rules/force-cn.txt
 # force-cn 是强制本地解析域名，force-nocn 是强制非本地解析域名
 
 echo "localhost 127.0.0.1" >> $v2dat_dir/rules/hosts.txt
